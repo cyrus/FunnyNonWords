679297def70457ab0521ee460f320a2e925a4803
Cyrus Shaoul <cyrus@landmark.edu>
2017-04-05 14:41:40 -0400

    Fixed serial code (again).

--- eegtrigger/test.serial.py
+++ eegtrigger/test.serial.py
@@ -2,15 +2,13 @@
 import serial
 import time
 ser = serial.Serial('/dev/cu.usbserial-ALHCTCQ',57600)
-print(ser.name)
-#items = [bytearray([1, 1, 1]),bytearray([1,1, 2]),bytearray([1,1, 3]),bytearray([1,1, 4])]
-#items = [bytearray(['\xff']), bytearray(['\x22'])
-#items = [bytearray([1]),bytearray([1]),bytearray([1]),bytearray([4])]
-#items = [b'x\03','2','1','2','1','2','1','2','1','2','1','2','1','2','1','2','1','2','1','2']
+print("Connected to: " + ser.name)
+# Each number needs to be in hex
 item1 = bytearray.fromhex(u'01')
 item2 = bytearray.fromhex(u'ff')
-items = [item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2,item1,item2]
+items = [item1,item2]
 for item in items:
     ser.write(item)
-    time.sleep(0.01)
+    time.sleep(0.2)
 ser.close()
+print("Finished!")
